58414a5e11f10d8c97db30aa8bb1997348fdfb85
Manohar Reddy <manoharreddychiluka1516@gmail.com>
2024-05-21 14:56:07 -0500

    factorial using for loop

--- src/ForClass.java
+++ src/ForClass.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ForClass {
-}
+import java.util.Scanner;
+
+public class ForClass {
+        public void method1(){
+            System.out.println("Enter a positive integer:");
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            System.out.println("The factorial of "+a+" is:");
+            int fact = 1;
+            for(int i=1;i<=a;i++){
+                fact = fact*i;
+            }
+            System.out.println(fact);
+        }
+    }
+
